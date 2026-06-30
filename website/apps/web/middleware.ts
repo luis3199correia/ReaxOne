e7ad926ed1e265bfa4177550be9c246f5c25eb1f
Luís Correia <luis.correia@linkconsulting.com>
2026-06-30 18:10:26 +0100

    fix: update middleware config to exclude sitemap and robots.txt from matcher

--- website/apps/web/middleware.ts
+++ website/apps/web/middleware.ts
@@ -80,5 +80,5 @@ export default function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|sitemap.xml|robots.txt).*)'],
 };
